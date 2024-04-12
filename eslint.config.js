d8112653939e0351acfe80b0fe51c1a6766ad82b
stdavis <stdavis@utah.gov>
2024-04-12 10:03:27 -0600

    fix: update to new eslint config filename

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+import globals from 'globals';
+import js from '@eslint/js';
+import prettier from 'eslint-config-prettier';
+
+export default [
+  js.configs.recommended,
+  prettier,
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      globals: {
+        ...globals.node,
+        ...globals.browser,
+        ...globals.jest,
+      },
+    },
+  },
+];
